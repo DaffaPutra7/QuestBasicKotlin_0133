5423bb2a525de892e860afaa4477de8cfbcc0c49
DaffaPutra7 <putradika3604@gmail.com>
2024-10-12 18:45:35 +0700

    Type Data pada Kotlin [1]

---
+++ app/src/main/java/com/example/questbasickotlin_0133/TypeData.kt
@@ -0,0 +1,21 @@
+package com.example.questbasickotlin_0133
+
+fun main( ) {
+    // Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    // Float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    // Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+
+}
