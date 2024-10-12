0f599149be7684b0402fa7fbed7d8d2e1ce76007
DaffaPutra7 <putradika3604@gmail.com>
2024-10-12 22:40:58 +0700

    Functions, code ini membahas Default Parameter Value

--- app/src/main/java/com/example/questbasickotlin_0133/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin_0133/Functions.kt
@@ -20,3 +20,24 @@ fun withNamedArgument(name: String, age: Int) {
     println("Hello, $name! You are $age years old.")
 }
 
+// Default Parameter Value
+// Default Parameter Value merupakan cara untuk memberikan nilai default pada parameter fungsi
+
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun  withReturn(panjang : Int, lebar : Int) {
+    return println(panjang*lebar)
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+
+}
+
