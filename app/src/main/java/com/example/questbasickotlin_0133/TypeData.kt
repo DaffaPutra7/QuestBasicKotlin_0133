28410cee442be086f08de59ec597b6a646d0a72d
DaffaPutra7 <putradika3604@gmail.com>
2024-10-12 18:49:54 +0700

    Type Data pada Kotlin [2]

--- app/src/main/java/com/example/questbasickotlin_0133/TypeData.kt
+++ app/src/main/java/com/example/questbasickotlin_0133/TypeData.kt
@@ -17,5 +17,18 @@ fun main( ) {
     val doubleValue: Double = 60.6
     println("Double: $doubleValue")
 
+    // Char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
 
+    // Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    // String
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin!"
+    println("String: $stringValue")
 }
