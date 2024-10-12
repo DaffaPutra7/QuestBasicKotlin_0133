0c0d5aa1e26ade5b42a68c698832e70c6502bb28
DaffaPutra7 <putradika3604@gmail.com>
2024-10-12 18:57:47 +0700

    Class pada Kotlin

--- app/src/main/java/com/example/questbasickotlin_0133/Class.kt
+++ app/src/main/java/com/example/questbasickotlin_0133/Class.kt
@@ -5,6 +5,7 @@ class Motor()
 class Contact(val id: Int, var email: String)
 
 fun main() {
+
     val contact = Contact(1, "mary@gmail.com")
 
     // Prints the value of the property: email
@@ -15,4 +16,5 @@ fun main() {
 
     // Prints the new value of the property: email
     println(contact.email)
+
 }
