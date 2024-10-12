2260127b2e373e7c45126c44b9fce1b9965a52e2
DaffaPutra7 <putradika3604@gmail.com>
2024-10-12 21:35:27 +0700

    Functions, code ini menggunakan Parameter dan tidak menggunakan Parameter. Dan juga membahas Named Argument

---
+++ app/src/main/java/com/example/questbasickotlin_0133/Functions.kt
@@ -0,0 +1,22 @@
+package com.example.questbasickotlin_0133
+
+fun withoutParameter() {
+    println("=== withoutParameter ===")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
+// Named Argument
+// Named Argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
